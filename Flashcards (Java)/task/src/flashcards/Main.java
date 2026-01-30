4dab012153330538f1d161d18feef62d3fee5fc6
Trayana Boykova <trayana.boykova@gmail.com>
2026-01-30 18:31:29 +0200

    completed stage 3

--- Flashcards (Java)/task/src/flashcards/Main.java
+++ Flashcards (Java)/task/src/flashcards/Main.java
@@ -6,14 +6,31 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String term = scanner.nextLine();        // read term
-        String definition = scanner.nextLine();  // read correct definition
-        String answer = scanner.nextLine();      // read user's answer
+        System.out.println("Input the number of cards:");
+        int n = Integer.parseInt(scanner.nextLine());
 
-        if (answer.equals(definition)) {
-            System.out.println("Your answer is right!");
-        } else {
-            System.out.println("Your answer is wrong...");
+        String[] terms = new String[n];
+        String[] definitions = new String[n];
+
+        // create cards
+        for (int i = 0; i < n; i++) {
+            System.out.println("Card #" + (i + 1) + ":");
+            terms[i] = scanner.nextLine();
+
+            System.out.println("The definition for card #" + (i + 1) + ":");
+            definitions[i] = scanner.nextLine();
+        }
+
+        // quiz user
+        for (int i = 0; i < n; i++) {
+            System.out.println("Print the definition of \"" + terms[i] + "\":");
+            String answer = scanner.nextLine();
+
+            if (answer.equals(definitions[i])) {
+                System.out.println("Correct!");
+            } else {
+                System.out.println("Wrong. The right answer is \"" + definitions[i] + "\".");
+            }
         }
     }
 }
