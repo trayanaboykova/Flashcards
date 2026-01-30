2f2d351d17cb10882daf04c066a4b719ff5903af
Trayana Boykova <trayana.boykova@gmail.com>
2026-01-30 18:21:30 +0200

    completed stage 1

--- Flashcards (Java)/task/src/flashcards/Main.java
+++ Flashcards (Java)/task/src/flashcards/Main.java
@@ -1,8 +1,17 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Card:");
+        String term = scanner.nextLine();
+        System.out.println(term);
+
+        System.out.println("Definition:");
+        String definition = scanner.nextLine();
+        System.out.println(definition);
     }
 }
